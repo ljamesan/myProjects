bb6cb49da04509c5ed2d4ea37d2b27eb39002f28
Ligory <ljamesan@hawk.iit.edu>
2019-01-04 09:37:25 -0500

    Sorting Update
    Sorting

--- SampleProject/src/com/java/Sorting.java
+++ SampleProject/src/com/java/Sorting.java
@@ -1,10 +1,10 @@
 package com.java;
 
 public class Sorting {
-
+	
 	public static void main(String[] args) {
 		
-		int[] arr = {5,4,3,7,9,2,1,8};
+		int[] arr = {8,4,3,7,9,2,1,8};
 		
 		//selectionSort(arr);
 		//bubbleSort(arr);
@@ -19,36 +19,47 @@ public class Sorting {
 		quickSort(arr, 0,arr.length-1);
 	}
 	private static void quickSort(int[] arr, int left, int right) {
-		if( right<=left) {
-			return;
+		if(left<right) {
+			int index  = partitionWithMiddleElementPivot(arr,left,right);
+			//int index  = partitionWithLastElementPivot(arr,left,right);
+			quickSort(arr,left,index);
+			quickSort(arr,index+1,right);
 		}
-		
-		int index  = partition(arr,left,right);
-		
-		quickSort(arr,left,index-1);
-		quickSort(arr,index+1,right);
 	}
-	private static int partition(int[] arr, int left, int right) {
-		int pivot = arr[(left+right)/2];
-		while(left < right) {
-			if(arr[left]<pivot) {
-				left++;
+	private static int partitionWithMiddleElementPivot(int[] arr, int left, int right) {
+		int pivot= arr[(left+right)/2];
+		while(true) {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+			if(left>=right) {
+				return left;
 			}
-			
-			if(arr[right]>pivot) {
-				right--;
-			}
-			
-			if(arr[left]>arr[right]) {
-				int temp=arr[left];
-				arr[left]=arr[right];
-				arr[right]=temp;
-			}
+			swap(arr,left,right);
+			left++;
+			right--;
 		}
-		
-		return left;
 	}
 	
+	
+	private static int partitionWithLastElementPivot(int[] arr, int low, int high) {
+		int pivot = arr[high];  
+	    int i = (low-1);  
+	    for (int j=low; j<high; j++) { 
+	        if (arr[j] <= pivot){ 
+	            i++; 
+	            swap(arr,i,j);
+	        } 
+	    }
+	    swap(arr,i+1,high);
+	    return i+1;
+	}
+	
+	private static void swap(int[] arr, int left, int right) {
+		int temp=arr[left];
+		arr[left]=arr[right];
+		arr[right]=temp;
+		
+	}
 	
 	public static void bubbleSort(int[] arr) {
 		boolean swap;
